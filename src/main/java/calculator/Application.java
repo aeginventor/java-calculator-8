b3a8ea760daa91babbae7c211d88ceaa38be602c
San <aeginventor@gmail.com>
2025-10-20 19:17:08 +0900

    feat: 덧셈 계산기 기본 입출력 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        // TODO: 계산기 로직 호출 (아직 로직이 없으므로 임시로 0을 반환)
+        int result = 0;
+
+        System.out.println("결과 : " + result);
     }
 }
