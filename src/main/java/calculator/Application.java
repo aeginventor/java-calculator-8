40d8b75ab28dadfe956773603488852056c168e1
San <aeginventor@gmail.com>
2025-10-20 21:36:45 +0900

    feat: 예외 발생 시 메시지 출력 및 애플리케이션 종료 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,9 +7,13 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.add(input);
+        try {
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.add(input);
 
-        System.out.println("결과 : " + result);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
